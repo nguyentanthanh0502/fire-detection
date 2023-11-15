dc6e8c47d157ee26027363769a78633535749e59
NTT <nguyentanthanh0502.ntt@gmail.com>
2023-11-16 00:28:44 +0700

    vercel

--- flask_yolo.py
+++ flask_yolo.py
@@ -37,4 +37,4 @@ def detect_objects():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
